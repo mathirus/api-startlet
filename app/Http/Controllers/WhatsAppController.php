3913fa0e35b4ab439a1e6712c9de5131a4ce41d0
AgustinChavero <agustindanielchavero@gmail.com>
2024-05-16 12:19:07 -0300

    try deploy

---
+++ app/Http/Controllers/WhatsAppController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\SendMessageWhatsAppRequest;
+use App\Services\WhatsAppService;
+use Illuminate\Http\Request;
+
+class WhatsAppController extends Controller
+{
+    protected $whatsappService;
+
+    public function __construct(WhatsAppService $whatsappService)
+    {
+        $this->whatsappService = $whatsappService;
+    }
+
+    public function sendMessage(SendMessageWhatsAppRequest $request)
+    {
+        $to = $request->input('to');
+        $message = $request->input('message');
+
+        $response = $this->whatsappService->sendMessage($to, $message);
+
+        return response()->json($response);
+    }
+}
---
+++ app/Services/WhatsAppService.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Services;
+
+use GuzzleHttp\Client;
+
+class WhatsAppService
+{
+    protected $client;
+    protected $baseUrl;
+    protected $token;
+
+    public function __construct()
+    {
+        $this->baseUrl = config('services.whatsapp.url');
+        $this->token = config('services.whatsapp.token');
+
+        $this->client = new Client([
+            'base_uri' => $this->baseUrl,
+            'headers' => [
+                'Authorization' => 'Bearer ' . $this->token,
+                'Content-Type' => 'application/json',
+            ],
+        ]);
+    }
+
+    public function sendMessage($to, $message)
+    {
+        $response = $this->client->post('', [
+            'json' => [
+                'messaging_product' => 'whatsapp',
+                'to' => $to,
+                'type' => 'text',
+                'text' => [
+                    'body' => $message,
+                ],
+            ],
+        ]);
+
+        return json_decode($response->getBody(), true);
+    }
+
+    public function initialMessage($to, $message)
+    {
+        $response = $this->client->post('', [
+            'json' => [
+                'messaging_product' => 'whatsapp',
+                'to' => $to,
+                'type' => 'template',
+                'template' => [
+                    'name' => 'hell_world',
+                    'code' => 'en_US'
+                ],
+            ],
+        ]);
+
+        return json_decode($response->getBody(), true);
+    }
+}
